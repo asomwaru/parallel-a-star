a15fb1661538be8e72eaf65d7d97b0ab147ac3e0
Michael <38502074+mplekunov@users.noreply.github.com>
2023-01-29 19:17:12 -0500

    Ref: Removed loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::{
     fs::{self, File},
-    io::Write,
-    thread,
-    time::Duration,
+    io::Write
 };
 
 use graph::{Vertex, WeightedEdge};
@@ -11,14 +9,10 @@ mod graph;
 mod maze;
 
 fn main() {
-    loop {
-        let maze: maze::Maze<Vertex<i32>, WeightedEdge<i32, Vertex<i32>>> = maze::Maze::new(30);
-        write_to_file("maze.txt".to_string(), &maze);
-        
-        // write_grid_to_file("test.txt".to_string(), &maze.render_maze());
+    let maze: maze::Maze<Vertex<i32>, WeightedEdge<i32, Vertex<i32>>> = maze::Maze::new(20);
+    write_to_file("maze.txt".to_string(), &maze);
 
-        thread::sleep(Duration::from_millis(40));
-    }
+    // write_grid_to_file("test.txt".to_string(), &maze.render_maze());
 }
 
 fn write_to_file<T: std::fmt::Display>(file_name: String, object: &T) {
@@ -30,7 +24,7 @@ fn write_to_file<T: std::fmt::Display>(file_name: String, object: &T) {
         .open(&file_name)
         .unwrap();
 
-        write!(fi, "{}", object).unwrap();
+    write!(fi, "{}", object).unwrap();
 }
 
 fn write_grid_to_file(file_name: String, grid: &Vec<Vec<String>>) {
