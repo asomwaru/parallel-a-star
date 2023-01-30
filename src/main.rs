9825ddb98c8210db446ac9be84322012eb1e000f
Michael <38502074+mplekunov@users.noreply.github.com>
2023-01-29 22:56:23 -0500

    Upd: updated algorithm, added colors

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 use std::{
-    fmt::Display,
     fs::{self, File},
     io::Write,
     thread,
     time::Duration,
 };
 
+use colored::Colorize;
 use graph::{Vertex, WeightedEdge};
 use path_finder::PathFinder;
-use Maze::Point2D;
+use Maze::{Point2D, MazeCell};
 
 mod Maze;
 mod graph;
 mod path_finder;
 
 fn main() {
-    let maze: Maze::Maze<Vertex<i32>, WeightedEdge<i32, Vertex<i32>>> = Maze::Maze::new(20);
+    loop {
+        let maze: Maze::Maze<Vertex<i32>, WeightedEdge<i32, Vertex<i32>>> = Maze::Maze::new(100);
 
-    let path_finder = PathFinder::new(maze.get_maze());
-    let maze_with_path = path_finder.show_path(path_finder.find_path(
-        Point2D::new(1, 1),
-        Point2D::new(37, 29),
-        true,
-        path_finder::SearchAlgorithms::BFS,
-    ));
+        let path_finder = PathFinder::new(maze.get_maze());
+        let maze_with_path = path_finder.show_path(path_finder.find_path(
+            Point2D::new(1, 1),
+            Point2D::new(199, 199),
+            true,
+            path_finder::SearchAlgorithms::BFS,
+        ));
 
-    write_to_file_grid("maze_with_path.txt".to_string(), maze_with_path);
+
+        write!(std::io::stdout(), "{}", to_string(&maze_with_path, true)).unwrap();
+        // write!(get_file("maze_with_path.txt".to_string()), "{}", to_string(&maze_with_path, false)).unwrap();
+
+        thread::sleep(Duration::from_millis(20));
+    }
 }
 
-fn write_to_file<T: Display>(file_name: String, object: &T) {
+fn get_file(file_name: String) -> File {
     File::create(&file_name).unwrap();
 
     let mut fi = fs::OpenOptions::new()
@@ -37,23 +43,35 @@ fn write_to_file<T: Display>(file_name: String, object: &T) {
         .open(&file_name)
         .unwrap();
 
-    write!(fi, "{}", object).unwrap();
+    return fi;
 }
 
-fn write_to_file_grid<T: Display>(file_name: String, grid: Vec<Vec<T>>) {
-    File::create(&file_name).unwrap();
-
-    let mut fi = fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(&file_name)
-        .unwrap();
+fn to_string<T>(grid: &Vec<Vec<MazeCell<T>>>, colored: bool) -> String {
+    let mut output = String::with_capacity(grid.len() * grid[0].len());
 
     grid.iter().for_each(|vector| {
         vector.iter().for_each(|val| {
-            fi.write_all(format!("{}", val).as_bytes()).unwrap();
+            let mut str = format!("{}", val);
+            
+            if colored {
+                str = match val.cell_type {
+                    Maze::CellType::Vertex => str,
+                    Maze::CellType::LeftRightBorder => str.red().to_string(),
+                    Maze::CellType::UpDownBorder => str.red().to_string(),
+                    Maze::CellType::Path => str.blue().to_string(),
+                    Maze::CellType::Start => str.blue().to_string(),
+                    Maze::CellType::End => str.blue().to_string(),
+                    Maze::CellType::None => str,
+                };
+            }
+
+            output.push_str(str.as_str());
         });
 
-        fi.write_all("\n".as_bytes()).unwrap();
+        output.push_str("\n");
     });
+
+    output.push_str("\n");
+
+    return output;
 }
